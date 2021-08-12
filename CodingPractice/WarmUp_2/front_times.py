0db16bbad3eb6781363438d8a68f0152ad95e607
Kenna <ikenna.ie1@gmail.com>
2021-08-12 02:54:10 +0100

    Pseudo for front times problem

---
+++ CodingPractice/WarmUp_2/front_times.py
@@ -0,0 +1,15 @@
+'''
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars,
+or whatever is there if the string is less than length 3. Return n copies of the front;
+
+front_times('Chocolate', 2) → 'ChoCho'
+front_times('Chocolate', 3) → 'ChoChoCho'
+front_times('Abc', 3) → 'AbcAbcAbc'
+'''
+
+#find the first 3 characters and store in variable c
+# loop through array: while iterator is less than n
+    #Add 3 characters to new variable
+    #increment iterator
+
+#return new variable
