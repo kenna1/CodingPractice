cc0e55de55f140a1a1d093793b7adc2fda0ecbe2
Kenna <ikenna.ie1@gmail.com>
2021-09-12 20:15:59 +0100

    second and second last letters removed solved

---
+++ CodingPractice/StringQs/without_end.py
@@ -0,0 +1,16 @@
+'''
+
+Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+
+without_end('Hello') → 'ell'
+without_end('java') → 'av'
+without_end('coding') → 'odin'
+
+'''
+
+def without_end(str):
+# return second to second last letter
+    return str[1:-1]
+
+print(without_end('Hello'))
