6bd9ccfacf2128204dac5dc3ef071f63ad206656
Kenna <ikenna.ie1@gmail.com>
2021-09-14 23:55:21 +0100

    list containing Pie solved

---
+++ CodingPractice/ListQs/make_pi.py
@@ -0,0 +1,15 @@
+'''
+
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+
+make_pi() → [3, 1, 4]
+'''
+
+def make_pi():
+#initialise an array with 1, 2, 3
+    arr = [3, 1, 4]
+# return it
+    return arr
+
+print(make_pi())
