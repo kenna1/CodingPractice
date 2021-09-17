3c82448cb4f57a5788c37745a626d1e2286e2de8
Kenna <ikenna.ie1@gmail.com>
2021-09-17 19:04:37 +0100

    new list containing middle values of two other lists solved

---
+++ CodingPractice/ListQs/middle_way.py
@@ -0,0 +1,20 @@
+'''
+
+Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+
+
+middle_way([1, 2, 3], [4, 5, 6]) → [2, 5]
+middle_way([7, 7, 7], [3, 8, 0]) → [7, 8]
+middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]
+'''
+
+def middle_way(a, b):
+#Find the middle variable
+    arr = []
+#Add it to new array
+    arr.append(a[1])
+    arr.append(b[1])
+#return new array
+    return arr
+
+print(middle_way([7, 7, 7], [3, 8, 0]))
