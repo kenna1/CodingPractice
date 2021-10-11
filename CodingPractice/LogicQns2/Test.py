8dd549d90619f13f618f71179d44031694471ef2
Kenna <ikenna.ie1@gmail.com>
2021-10-11 21:36:26 +0100

    Testing list comprehensions

---
+++ CodingPractice/LogicQns2/Test.py
@@ -0,0 +1,19 @@
+
+def result():
+    sqlist= [x*x for x in range(1, 11) if x%2 != 0]
+    word = [l.upper() for l in 'comprehension' if l not in 'aeiou']
+    wordlist = ['cat', 'dog', 'rabbit']
+    letterlist = []
+    #word2 = [letterlist.append(aletter) for aword in wordlist for aletter in word]
+    word2 = [word[i] for word in wordlist for i in range(len(word)) if word[i] not in ]
+
+    print(word2)
+
+result()
+'''
+wordlist = ['cat','dog','rabbit']
+letterlist = []
+for aword in wordlist:
+    for aletter in aword:
+        letterlist.append(aletter)
+print(letterlist)'''
